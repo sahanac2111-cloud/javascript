cf04b8e140b4a2279129e2197b2c54b974f68df7
sahanac2111-cloud <sahanac2111@gmail.com>
2026-06-26 12:39:27 +0530

    Add files via upload

---
+++ variable6.js
@@ -0,0 +1,10 @@
+let name = "Sana";
+const college = "TCE";
+let year = 2;
+
+console.log(name);
+console.log(college);
+console.log(year);
+console.log(typeof name);     // what type is it?
+console.log(typeof year);
+console.log(typeof true);
---
+++ variable7.js
@@ -0,0 +1,6 @@
+let name="sanah";
+let collage="TCE";
+const year=1;
+console.log(name);
+console.log(collage);
+console.log(year);
