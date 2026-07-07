016ffee9541ebfc969f49d2ef87b9d28fb60da4a
sahanac2111-cloud <sahanac2111@gmail.com>
2026-07-07 21:54:05 +0530

    Add files via upload

---
+++ loop7.js
@@ -0,0 +1,7 @@
+let fact=1;
+for(let i=1;i<=5;i++){
+
+    
+    fact=fact*i;
+}
+    console.log(fact);
---
+++ loop8.js
@@ -0,0 +1,8 @@
+let count=0;
+for(let i=1;i<=30;i++){
+   if(i%3==0){
+    count++;
+   }
+}
+console.log(count);
+
